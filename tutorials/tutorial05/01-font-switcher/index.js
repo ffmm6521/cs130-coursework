e4e14b04feadc003e5c6a748d65a766a13bc07e5
minfei <ffmm6521@gmail.com>
2022-05-02 22:58:51 -0700

    bug fix

--- tutorials/tutorial05/01-font-switcher/index.js
+++ tutorials/tutorial05/01-font-switcher/index.js
@@ -1,12 +1,18 @@
 const makeBigger = () => {
-   alert('make bigger!');
+   // alert('make bigger!');
+   var computedFontSize = window.getComputedStyle(document.querySelector(".content")).fontSize;
+   var fontSize  = parseInt(computedFontSize) + 10 + "px";
+   document.querySelector(".content").style.fontSize = fontSize 
 };
 
 const makeSmaller = () => {
-   alert('make smaller!');
+   // alert('make smaller!');
+   var computedFontSize = window.getComputedStyle(document.querySelector(".content")).fontSize;
+   var fontSize  = parseInt(computedFontSize) -10 + "px";
+   document.querySelector(".content").style.fontSize = fontSize 
 };
 
-/*
-document.querySelector(???).addEventListener('click', makeBigger);
-document.querySelector(???).addEventListener('click', makeSmaller);
-*/
+
+document.querySelector("#a1").addEventListener('click', makeBigger);
+document.querySelector("#a2").addEventListener('click', makeSmaller);
+
--- tutorials/tutorial05/02-theme-switcher/index.js
+++ tutorials/tutorial05/02-theme-switcher/index.js
@@ -5,3 +5,32 @@
     2. Modify the className property of the body tag 
        based on the button that was clicked.
 */
+
+const Desert = () => {
+   // alert('make smaller!');
+
+   document.querySelector("body").className = 'desert'
+};
+const Ocean = () => {
+   // alert('make smaller!');
+
+   document.querySelector("body").className = 'ocean'
+};
+const High_contrast = () => {
+   // alert('make smaller!');
+
+   document.querySelector("body").className = 'high-contrast'
+};
+
+const defult = () => {
+   // alert('make smaller!');
+
+   // document.querySelector("body").className = 'None'
+   document.querySelector("body").removeAttribute('class')
+   
+};
+
+document.querySelector("#desert").addEventListener('click', Desert);
+document.querySelector("#ocean").addEventListener('click', Ocean);
+document.querySelector("#high-contrast").addEventListener('click', High_contrast);
+document.querySelector("#default").addEventListener('click', defult);
