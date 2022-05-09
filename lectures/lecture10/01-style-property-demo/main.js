ed6f34039eb9da3a636e13600b283cd1a6038587
minfei <ffmm6521@gmail.com>
2022-05-08 23:27:34 -0700

    tut6

---
+++ lectures/lecture10/01-style-property-demo/main.js
@@ -0,0 +1,24 @@
+
+const makeRed = () => {
+    document.querySelector('#btn1').style.backgroundColor = 'red';
+    document.querySelector('body').style.backgroundColor = 'red';
+
+    console.log('Change background to red');
+};
+
+const makeBlue = () => {
+    // your code here...
+    document.querySelector('body').style.backgroundColor = 'blue';
+    console.log('Change background to blue');
+};
+
+const makePink = () => {
+    document.querySelector('body').style.backgroundColor = 'pink';
+    console.log('Change background to pink');
+};
+
+const makeOrange = () => {
+    document.querySelector('body').style.backgroundColor = 'orange';
+    console.log('Change background to orange');
+};
+
